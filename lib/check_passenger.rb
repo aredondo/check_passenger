40f11855db2419e980ea44f143c540d73662227f
Alvaro Redondo <alvaro@redondo.name>
2014-09-12 00:12:07 +0200

    Implement wrapper class to run passenger-status

--- lib/check_passenger.rb
+++ lib/check_passenger.rb
@@ -1,6 +1,7 @@
 require 'check_passenger/parser'
-require 'check_passenger/version'
+require 'check_passenger/passenger_status'
 require 'check_passenger/status_output_error'
+require 'check_passenger/version'
 
 module CheckPassenger
   LIVE_PROCESS_TTL_IN_SECONDS = 300
---
+++ lib/check_passenger/passenger_status.rb
@@ -0,0 +1,29 @@
+module CheckPassenger
+  class PassengerStatus
+    class << self
+      attr_accessor :path
+
+      def run
+        `#{passenger_status_executable_path}`
+      end
+
+      private
+
+      def passenger_status_executable_path
+        command = if @path
+          if File.directory?(@path)
+            File.expand_path('passenger-status', @path)
+          else
+            @path
+          end
+        else
+          `which passenger-status`
+        end
+
+        raise Errno::ENOENT, 'Cannot find passenger-status' unless File.executable?(command)
+
+        command
+      end
+    end
+  end
+end
