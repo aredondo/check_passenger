5fec62fc976b7ce25e80327ab82c0ae3389bfd55
Alvaro Redondo <alvaro@redondo.name>
2014-09-11 02:34:19 +0200

    Use minitest-reporters for testing output format

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,6 @@ $LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
 require 'check_passenger'
 require 'minitest'
 require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
