8efe272aabc036fbcd7d0554ef6f1c7d447a4702
Alvaro Redondo <alvaro@redondo.name>
2014-09-12 18:11:05 +0200

    Edit code

--- lib/check_passenger.rb
+++ lib/check_passenger.rb
@@ -1,9 +1,10 @@
-require 'check_passenger/parser'
-require 'check_passenger/passenger_status'
-require 'check_passenger/status_output_error'
-require 'check_passenger/version'
+%w{
+  parser
+  passenger_status
+  status_output_error
+  version
+}.each { |lib| require 'check_passenger/' + lib }
 
 module CheckPassenger
   LIVE_PROCESS_TTL_IN_SECONDS = 300
-  # Your code goes here...
 end
