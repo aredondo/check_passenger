29f8b7150644b2d30dd07d4b31b69ea4272bcf94
Alvaro Redondo <alvaro@redondo.name>
2015-10-01 11:35:45 +0200

    Add Code Climate to testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 lib = File.expand_path('../lib', File.dirname(__FILE__))
 $LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
 
